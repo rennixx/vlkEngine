33557920c43d1282e0bf44cdc5b7cea5d31e4bfe
rennixx <ensw05323@student.su.edu.krd>
2026-01-28 10:00:59 +0300

    Add Vulkan descriptor set management header

---
+++ src/renderer/descriptor.h
@@ -0,0 +1,21 @@
+/**
+ * @file descriptor.h
+ * @brief Vulkan descriptor set management
+ */
+
+#ifndef VE_DESCRIPTOR_H
+#define VE_DESCRIPTOR_H
+
+#include "vulkan_core.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* TODO: Implement descriptor management */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VE_DESCRIPTOR_H */
