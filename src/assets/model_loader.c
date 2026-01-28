42ec3f5aabe3edc0ec479d58792fe4610da0d931
rennixx <ensw05323@student.su.edu.krd>
2026-01-28 10:06:21 +0300

    Add initial implementation files for model loading

---
+++ src/assets/model_loader.c
@@ -0,0 +1,8 @@
+/**
+ * @file model_loader.c
+ * @brief glTF/OBJ model loading implementation
+ */
+
+#include "model_loader.h"
+
+/* TODO: Implement model loading */
---
+++ src/assets/model_loader.h
@@ -0,0 +1,21 @@
+/**
+ * @file model_loader.h
+ * @brief glTF/OBJ model loading
+ */
+
+#ifndef VE_MODEL_LOADER_H
+#define VE_MODEL_LOADER_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* TODO: Implement model loading */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VE_MODEL_LOADER_H */
