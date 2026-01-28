82d7207a53e8f2c8dd25292409807d2b4e46156b
rennixx <ensw05323@student.su.edu.krd>
2026-01-28 10:06:59 +0300

    Add initial implementation files for texture loading

---
+++ src/assets/texture_loader.c
@@ -0,0 +1,8 @@
+/**
+ * @file texture_loader.c
+ * @brief Texture loading implementation
+ */
+
+#include "texture_loader.h"
+
+/* TODO: Implement texture loading */
---
+++ src/assets/texture_loader.h
@@ -0,0 +1,21 @@
+/**
+ * @file texture_loader.h
+ * @brief Texture loading
+ */
+
+#ifndef VE_TEXTURE_LOADER_H
+#define VE_TEXTURE_LOADER_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* TODO: Implement texture loading */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VE_TEXTURE_LOADER_H */
