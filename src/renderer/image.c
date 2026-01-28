f293188caa26f120d6f622d496d88e91ca6c67cf
rennixx <ensw05323@student.su.edu.krd>
2026-01-28 10:02:19 +0300

    Add Vulkan image and texture management header and implementation

---
+++ src/renderer/image.c
@@ -0,0 +1,8 @@
+/**
+ * @file image.c
+ * @brief Vulkan image and texture management implementation
+ */
+
+#include "image.h"
+
+/* TODO: Implement image management */
---
+++ src/renderer/image.h
@@ -0,0 +1,21 @@
+/**
+ * @file image.h
+ * @brief Vulkan image and texture management
+ */
+
+#ifndef VE_IMAGE_H
+#define VE_IMAGE_H
+
+#include "vulkan_core.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* TODO: Implement image management */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VE_IMAGE_H */
