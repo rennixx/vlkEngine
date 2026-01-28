afab2138b47b750ee3aae31dd9cebdf0378422d3
rennixx <ensw05323@student.su.edu.krd>
2026-01-28 10:00:40 +0300

    Add Vulkan graphics pipeline management header

---
+++ src/renderer/pipeline.c
@@ -0,0 +1,8 @@
+/**
+ * @file pipeline.c
+ * @brief Vulkan graphics pipeline management implementation
+ */
+
+#include "pipeline.h"
+
+/* TODO: Implement pipeline management */
---
+++ src/renderer/pipeline.h
@@ -0,0 +1,21 @@
+/**
+ * @file pipeline.h
+ * @brief Vulkan graphics pipeline management
+ */
+
+#ifndef VE_PIPELINE_H
+#define VE_PIPELINE_H
+
+#include "vulkan_core.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* TODO: Implement pipeline management */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VE_PIPELINE_H */
