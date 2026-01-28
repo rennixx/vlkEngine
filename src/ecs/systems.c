ab6a20f1cab14bb465cae75f4ca7310dab8dff7f
rennixx <ensw05323@student.su.edu.krd>
2026-01-28 10:03:58 +0300

    Add initial implementation files for built-in ECS systems

---
+++ src/ecs/systems.c
@@ -0,0 +1,8 @@
+/**
+ * @file systems.c
+ * @brief Built-in ECS systems implementation
+ */
+
+#include "systems.h"
+
+/* TODO: Implement built-in systems */
---
+++ src/ecs/systems.h
@@ -0,0 +1,21 @@
+/**
+ * @file systems.h
+ * @brief Built-in ECS systems
+ */
+
+#ifndef VE_SYSTEMS_H
+#define VE_SYSTEMS_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* TODO: Implement built-in systems */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VE_SYSTEMS_H */
