5882105434989e0ae84ee9f3fd22b853e1bd7902
rennixx <ensw05323@student.su.edu.krd>
2026-01-28 10:05:48 +0300

    Add initial implementation files for asset management

---
+++ src/assets/asset_manager.c
@@ -0,0 +1,8 @@
+/**
+ * @file asset_manager.c
+ * @brief Asset management implementation
+ */
+
+#include "asset_manager.h"
+
+/* TODO: Implement asset management */
---
+++ src/assets/asset_manager.h
@@ -0,0 +1,21 @@
+/**
+ * @file asset_manager.h
+ * @brief Asset management
+ */
+
+#ifndef VE_ASSET_MANAGER_H
+#define VE_ASSET_MANAGER_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* TODO: Implement asset management */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VE_ASSET_MANAGER_H */
