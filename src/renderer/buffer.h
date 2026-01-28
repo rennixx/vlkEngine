2e5051666361cecdd983dcd7cf40674b66ceba9f
rennixx <ensw05323@student.su.edu.krd>
2026-01-28 10:01:35 +0300

    Add Vulkan buffer management header

---
+++ src/renderer/buffer.h
@@ -0,0 +1,21 @@
+/**
+ * @file buffer.h
+ * @brief Vulkan buffer management
+ */
+
+#ifndef VE_BUFFER_H
+#define VE_BUFFER_H
+
+#include "vulkan_core.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* TODO: Implement buffer management */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VE_BUFFER_H */
