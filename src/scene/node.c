52f6c013715fba42da13c8290e15095b8c1800b1
rennixx <ensw05323@student.su.edu.krd>
2026-01-28 10:04:40 +0300

    Add initial implementation files for scene node management

---
+++ src/scene/node.c
@@ -0,0 +1,8 @@
+/**
+ * @file node.c
+ * @brief Scene node management implementation
+ */
+
+#include "node.h"
+
+/* TODO: Implement scene node management */
---
+++ src/scene/node.h
@@ -0,0 +1,21 @@
+/**
+ * @file node.h
+ * @brief Scene node management
+ */
+
+#ifndef VE_NODE_H
+#define VE_NODE_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* TODO: Implement scene node management */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VE_NODE_H */
